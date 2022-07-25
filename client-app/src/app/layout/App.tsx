750cb3c8e67e059a9634ee28b70bdac372a30bc7
Bryan <sauc3r@outlook.com>
2022-07-24 20:55:06 -0500

    Set up initial folder structure. Create an Activity interface for type safety.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
-    const [activities, setActivities] = useState([]);
+    const [activities, setActivities] = useState<Activity[]>([]);
     useEffect(() => {
-        axios.get('http://localhost:5000/api/activities').then(response => {
+        axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
             setActivities(response.data);
         })
     }, [])
@@ -15,7 +15,7 @@ function App() {
         <div>
             <Header as='h1' icon='users' content='Personas'/>
             <List>
-                {activities.map((activity: any) => (
+                {activities.map(activity => (
                     <List.Item key={activity.id}>
                         {activity.title}
                     </List.Item>
